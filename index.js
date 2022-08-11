05a5ce39758aae63830e19955f610a13c3f75d79
Hyeong-Joon <gudwns1020@naver.com>
2022-08-12 04:11:21 +0900

    Import Express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello NailedIt");
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
