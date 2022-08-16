94fd026a23202ba71eea7895ae80f33a506d6d17
Hyeong-Joon <gudwns1020@naver.com>
2022-08-16 17:22:52 +0900

    Use Morgan

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+
+app.use(logger);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on port http://localhost:${PORT}🚀`);
 
 app.listen(PORT, handleListening);
